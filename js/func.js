f06e178359f94f11ba4c2d122732b5a5caa2082d
Pastor-git <maciej.pastor@gmail.com>
2021-02-14 21:39:27 +0100

    Valentines update

--- js/func.js
+++ js/func.js
@@ -6,7 +6,9 @@ document.getElementById("getSalary").addEventListener("click",licz);
 function licz (e) {
     console.log(e);
     e.preventDefault();
+    
 
+    console.log("hello")
     const salary = document.getElementById("salary").value;
     if (salary >= av_salary) {
         document.getElementById("result").innerText="jest dobrze"
